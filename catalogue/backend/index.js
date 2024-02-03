89b5cc215a467f30b48a39714e0b9f26d7252a46
Che Bernice Bih A <cheberniceworks@gmail.com>
2024-02-03 11:16:26 +0100

    Another commit 4: catalogue

--- catalogue/backend/index.js
+++ catalogue/backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://catalogue-7ww5g2nl9-che-bernices-projects.vercel.app"],
         methods: ["POST", "GET", "DELETE"],
         credentials: true
     }
